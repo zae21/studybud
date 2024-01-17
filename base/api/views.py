9e2af5a684599d52cf2e1f4b4bee4b951ff9bc46
zae21 <zepra21@proton.me>
2024-01-17 16:30:05 +0700

    add rest framewrok + cors headers

--- base/api/views.py
+++ base/api/views.py
@@ -1,7 +1,25 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomsSerializer
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
+        'GET /api',
         'GET /api/rooms',
+        'GET /api/rooms/:id',
     ]
-    return JsonResponse()
+    return Response(routes)
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomsSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomsSerializer(room, many=False)
+    return Response(serializer.data)
--- studybud/settings.py
+++ studybud/settings.py
@@ -38,17 +38,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # ===========================
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
+    # ===========================
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # =====================================================
+    'corsheaders.middleware.CorsMiddleware',
+    # =====================================================
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'studybud.urls'
@@ -131,3 +138,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
